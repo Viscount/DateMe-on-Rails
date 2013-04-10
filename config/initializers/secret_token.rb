e5345d72986817892ab590e4663cb2ec7168d65a
廖振宇 <maple01@126.com>
2013-04-10 08:02:04 +0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DateMe::Application.config.secret_token = '138cf0cf198a1ae77d1db1fc057571e475a4097ad95edf18daab41fbad23a15c20c6744ff285afdd792e4126de30f35bbcea8a9663b15fd59e9bee6adeb11578'
